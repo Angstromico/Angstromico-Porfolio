9bb676d382a06b97410a4b34899c54ff9a58b845
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-10-03 00:06:07 -0400

    Index heroku

--- index.php
+++ index.php
@@ -260,7 +260,7 @@ if(isset($_POST['submit'])){
     
         </div>
    <div class="container-contact">
-  <form class="contact" method="POST" id="form" action="index.php">
+  <form class="contact" method="POST" id="form" action="https://manuel-morales-portfolio.herokuapp.com/">
     <p>Write Me</p>
     <input type="name" placeholder="Name" class="input" name="name" required><br>
     <input type="email" placeholder="email" class="input" name="email" required><br>
