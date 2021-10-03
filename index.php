504b3ce5553f90187f9d0eaaecdaa5adf27af582
Manuel Esteban Morales Zuarez <79806775+Angstromico@users.noreply.github.com>
2021-10-03 10:33:03 -0400

    Better Hover

--- index.php
+++ index.php
@@ -282,13 +282,13 @@ if(isset($_POST['submit'])){
         <div class="container-icon">
             <a href="https://twitter.com/CodingManuel" target="_blank" class="twitter"><i class="fab fa-twitter"></i></a>
         </div>
-        <div class="social-networks">
+        <div class="social-network">
             <a href="https://www.facebook.com/esteban.zuarez.14/" target="_blank" class="facebook"><i class="fab fa-facebook"></i></a>
         </div>
-        <div class="social-networks">
+        <div class="social-network">
             <a href="https://www.instagram.com/manuelmorales4280/" target="_blank" class="instagram"><i class="fab fa-instagram"></i></a>
         </div>
-        <div class="social-networks">
+        <div class="social-network">
             <a href="https://github.com/Angstromico" target="_blank" class="github"><i class="fab fa-github"></i></a>
         </div>
         </div>
